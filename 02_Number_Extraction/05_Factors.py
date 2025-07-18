31c5449b8318c1bdc9460c89a539d845ff024180
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-07-18 23:31:59 +0500

    good solution

--- 02_Number_Extraction/05_Factors.py
+++ 02_Number_Extraction/05_Factors.py
@@ -6,10 +6,21 @@ def printFactors(num):
     return factors
 
 
+def goodSolutionFactors(num):
+    factors = []
+    for i in range(1, num // 2 + 1):
+        if num % i == 0:
+            factors.append(i)
+    factors.append(num)
+    return factors
+
+
 def main():
-    n = 43
+    n = 20
     numFactors = printFactors(n)
     print(numFactors)
+    goodNumFactors = goodSolutionFactors(n)
+    print(goodNumFactors)
 
 
 if __name__ == "__main__":
