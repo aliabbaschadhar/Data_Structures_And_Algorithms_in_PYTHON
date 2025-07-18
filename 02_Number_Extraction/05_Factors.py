34ea5b191d2eb57c48545943f68bc016dc4261f4
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-07-18 23:52:35 +0500

    optimal solution

--- 02_Number_Extraction/05_Factors.py
+++ 02_Number_Extraction/05_Factors.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 def printFactors(num):
     factors = []
     for i in range(1, num + 1):
@@ -15,12 +18,31 @@ def goodSolutionFactors(num):
     return factors
 
 
+def optimalSolutionFactors(num):
+    factors = []
+    for i in range(1, int(sqrt(num)) + 1):
+        if num % i == 0:
+            factors.append(i)
+            if num // i != i:
+                factors.append(num // i)
+    factors.sort()
+    return factors
+
+    # 1----36
+    # 2----18
+    # 3----12
+    # 4----9
+    # 6----6
+
+
 def main():
     n = 20
     numFactors = printFactors(n)
     print(numFactors)
     goodNumFactors = goodSolutionFactors(n)
     print(goodNumFactors)
+    optimalNumFactors = optimalSolutionFactors(n)
+    print(optimalNumFactors)
 
 
 if __name__ == "__main__":
